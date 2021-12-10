8b83349ef7dac2d8a533d52802224cdde5075215
unknown <sargey7@gmail.com>
2021-12-10 13:51:23 +0200

    101221

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-import './homepage.styles.scss'
-
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
