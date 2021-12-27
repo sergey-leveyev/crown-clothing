af67a90dee344b16595244e6ddae1f932ae856b0
unknown <sargey7@gmail.com>
2021-12-27 15:04:15 +0200

    271221

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import Auto from "./pages/Auto/Auto";
 
 function App() {
   return (
-   <div>
+    <div>
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
+        <Route path="/signin" element={<Auto />} />
       </Routes>
-   </div>
-  )
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Auto/Auto.js
@@ -0,0 +1,13 @@
+import "./Auto.scss";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+const Auto = () => {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+};
+
+export default Auto;
