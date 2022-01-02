1dbedb2628d104f4e67e49d2a66a2237cb5f2d2f
unknown <sargey7@gmail.com>
2022-01-02 10:22:17 +0200

    020122

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,13 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Firestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
+  apiKey: "AIzaSyCjIdOBQjgMTqcQpDYUJujQqrT0_cmCEG0",
+  authDomain: "crwn-db-59b2d.firebaseapp.com",
+  projectId: "crwn-db-59b2d",
+  storageBucket: "crwn-db-59b2d.appspot.com",
+  messagingSenderId: "505278827618",
+  appId: "1:505278827618:web:9c7ef6b70cf674647d953b",
+  measurementId: "G-M7C9G6PTKR",
 };
 
 export const createUserProfileDocument = async (userAuth, addittionalData) => {
